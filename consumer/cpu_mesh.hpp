c939f2c5c1de24a4b0c0299689540320f7dc95ee
Torbjörn Rathsman <mialsudril@gmail.com>
2022-09-07 10:20:04 +0200

    Add point class

--- consumer/cpu_mesh.hpp
+++ consumer/cpu_mesh.hpp
@@ -10,6 +10,33 @@ namespace playback
 		float v;
 	};
 	
+	using vec4_t [[gnu::vector_size(16)]] = float;
+	
+	class point
+	{
+	public:
+		using value_type = vec4_t;
+		
+		constexpr point() = default; // so we can load from file and set w afterwards
+		
+		constexpr explicit point(float x, float y, float z): m_value{x, y, z, 1.0f}
+		{}
+		
+		constexpr vec4_t value() const
+		{ return m_value; }
+		
+		constexpr void set_w()
+		{ m_value[3] = 1.0f; }
+		
+	private:
+		vec4_t m_value;
+	};
+	
+	constexpr auto origin()
+	{
+		return point{0.0f, 0.0f, 0.0f};
+	}
+	
 	template<class VertexType, class IndexType>
 	class cpu_mesh
 	{
