2fdb0bd93f172bcdaddbcf98412303e184e52fc8
Torbjörn Rathsman <mialsudril@gmail.com>
2022-09-08 18:31:11 +0200

    Add coordinates to image_plane

--- consumer/gl_video_port.hpp
+++ consumer/gl_video_port.hpp
@@ -11,9 +11,23 @@ namespace playback
 	
 	inline constexpr mesh<array_of_4, std::array<unsigned int, 6>> image_plane
 	{
-		array_of_4<point>{},
-		array_of_4<uv_coords>{},
-		std::array<unsigned int, 6>{}
+		array_of_4<point>
+		{
+			point{ 1.0f,  1.0f, 0.0f},
+			point{ 1.0f, -1.0f, 0.0f},
+			point{-1.0f, -1.0f, 0.0f},
+			point{-1.0f,  1.0f, 0.0f}
+		},
+
+		array_of_4<uv_coords>
+		{
+			uv_coords{ 1.0f,  0.0f},
+			uv_coords{ 1.0f,  1.0f},
+			uv_coords{ 0.0f,  1.0f},
+			uv_coords{ 0.0f,  0.0f}						
+		},
+
+		std::array<unsigned int, 6>{0, 3, 1, 3, 2, 1}
 	};
 	
 	class gl_video_port
