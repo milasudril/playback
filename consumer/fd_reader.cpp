ea577cac5c7387cd86ec9d84b666d9c0d2605871
Torbjörn Rathsman <milasudril@gmail.com>
2022-09-11 19:52:59 +0200

    Implemented fd_reader class

---
+++ consumer/fd_reader.cpp
@@ -0,0 +1,45 @@
+//@	{"target":{"name":"fdreader.o"}}
+
+#include "./fd_reader.hpp"
+
+#include <unistd.h>
+
+
+playback::read_data_result playback::fd_reader::read_into(std::vector<std::byte>& buffer,
+	size_t bytes_to_read)
+{
+	while(bytes_to_read != 0)
+	{
+		if(m_read_ptr == m_end_ptr)
+		{
+			if(auto const res = fetch(); res != read_status::success) [[unlikely]]
+			{ return read_data_result{bytes_to_read, res}; }
+		}
+
+		auto const n = std::min(static_cast<size_t>(m_end_ptr - m_read_ptr),
+			bytes_to_read);
+		std::copy_n(m_read_ptr, n, std::back_inserter(buffer));
+		m_end_ptr += n;
+		bytes_to_read -= n;
+	}
+
+	return read_data_result{0, read_status::success};
+}
+
+playback::read_status playback::fd_reader::fetch()
+{
+	auto res = ::read(m_fd, std::data(m_buffer), std::size(m_buffer));
+	switch(res)
+	{
+		case 0:
+			return read_status::eof;
+
+		case -1:
+			return read_status::blocked;
+
+		default:
+			m_read_ptr = std::data(m_buffer);
+			m_end_ptr = m_read_ptr + res;
+			return read_status::success;
+	}
+}
--- consumer/fd_reader.hpp
+++ consumer/fd_reader.hpp
@@ -1,5 +1,14 @@
+//@	{"dependencies_extra":[{"ref":"./fdreader.o", "rel":"implementation"}]}
+
 #ifndef PLAYBACK_FDREADER_HPP
 #define PLAYBACK_FDREADER_HPP
+
+#include <sys/types.h>
+
+#include <cstdint>
+#include <cstddef>
+#include <array>
+#include <vector>
 
 namespace playback
 {
@@ -9,65 +18,77 @@ namespace playback
 	{
 		std::byte value;
 		read_status status;
+	};
+
+	inline auto make_read_result(ssize_t val)
+	{
+		return read_byte_result{
+			.value = std::byte{0x0},
+			.status = val == 0 ? read_status::eof : read_status::blocked
+		};
 	}
+
+	inline auto make_read_result(std::byte val)
+	{
+		return read_byte_result{.value = val, .status = read_status::success};
+	}
+
+
+
+	struct read_data_result
+	{
+		size_t bytes_to_read;
+		read_status status;
+	};
+
+	inline auto make_read_result(size_t bytes_to_read, ssize_t val)
+	{
+		return read_data_result{
+			.bytes_to_read = bytes_to_read,
+			.status = val == 0 ? read_status::eof : read_status::blocked
+		};
+	}
+
 
 	class fd_reader
 	{
 	public:
-		read_buffer(int fd):
-			m_fd{fd}
-			m_read_ptr{std::data(m_buffer)}
+		explicit fd_reader(int fd):
+			m_fd{fd},
+			m_read_ptr{std::data(m_buffer)},
 			m_end_ptr{std::data(m_buffer)}
 		{}
 
-		read_buffer(read_buffer const&) = delete;
-		read_buffer(read_buffer&&) = delete;
-		read_buffer& operator=(read_buffer const&) = delete;
-		read_buffer& operator=(read_buffer&&) = delete;
+		fd_reader(fd_reader const&) = delete;
+		fd_reader(fd_reader&&) = delete;
+		fd_reader& operator=(fd_reader const&) = delete;
+		fd_reader& operator=(fd_reader&&) = delete;
 
-		auto read_byte()
+		read_byte_result read_byte()
 		{
-			if(m_read_ptr == m_ptr)
+			if(m_read_ptr == m_end_ptr) [[unlikely]]
 			{
-				auto res = fetch();
-				if(res < 0)
+				if(auto const res = fetch(); res != read_status::success) [[unlikely]]
 				{
-					return make_read_status(res);
+					return read_byte_result{std::byte{0x00}, res};
 				}
 			}
 
 			auto ret = *m_read_ptr;
 			++m_read_ptr;
-			return make_read_status(ret);
+			return read_byte_result{ret, read_status::success};
 		}
 
-		auto read_into(std::vector<std::byte>& buffer, size_t bytes_to_read)
-		{
-			while(bytes_to_read != 0)
-			{
-				if(m_read_ptr == m_ptr)
-				{
-					auto res = fetch();
-					if(res <= 0)
-					{ return make_read_result(bytes_to_read, res); }
-				}
-
-				auto const n = std::min(static_cast<size_t>(m_end_ptr - m_read_ptr),
-					bytes_to_read);
-				std::copy_n(m_read_ptr, n, std::back_inserter(buffer));
-				m_end_ptr += n;
-				bytes_to_read -= n;
-			}
-
-			return make_read_result(bytes_to_read);
-		}
+		read_data_result read_into(std::vector<std::byte>& buffer, size_t bytes_to_read);
 
 	private:
+		read_status fetch();
+
 		int m_fd;
 		std::array<std::byte, 65536> m_buffer;
 		std::byte const* m_read_ptr;
 		std::byte const* m_end_ptr;
-	}
+	};
 }
 
 #endif
