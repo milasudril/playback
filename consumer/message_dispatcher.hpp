536171b7cbc50051ef245462b9ce704ea449a6d5
Torbjörn Rathsman <milasudril@gmail.com>
2022-09-17 17:44:55 +0200

    Adding queue algo

--- consumer/message_dispatcher.hpp
+++ consumer/message_dispatcher.hpp
@@ -6,10 +6,7 @@
 #include "./gl_video_device.hpp"
 
 #include <anon/object.hpp>
-
-#include <span>
-#include <string_view>
-#include <chrono>
+#include <queue>
 
 namespace playback
 {
@@ -17,7 +14,8 @@ namespace playback
 	{
 	public:
 		explicit message_dispatcher(gl_video_device& video_out):
-		m_video_out{video_out}
+		m_video_out{video_out},
+		m_last_event{std::chrono::steady_clock::now()}
 		{}
 
 		void dispatch(command const& cmd)
@@ -25,11 +23,41 @@ namespace playback
 			if(cmd.message_type_name == "video_frame_update")
 			{
 				auto const vfu = deserialize(empty<video_frame_update>{}, cmd.content);
-				m_video_out.get().set_pixels(vfu.video_port, cmd.payload);			}
+				m_video_out.get().set_pixels(vfu.video_port, cmd.payload);
+			}
+		}
+
+		void enqueue(command&& cmd)
+		{
+			m_commands.push(std::move(cmd));
+		}
+
+		void flush_expired_commands(std::chrono::steady_clock::time_point now)
+		{
+			auto last_event = m_last_event;
+			while(!m_commands.empty())
+			{
+				auto const delay = m_commands.front().delay;
+				auto const current_event = last_event + delay;
+				if(current_event <= now)
+				{
+					auto cmd = std::move(m_commands.front());
+					m_commands.pop();
+					last_event = current_event;
+					dispatch(cmd);
+				}
+				else
+				{
+					m_last_event = last_event;
+					return;
+				}
+			}
 		}
 
 	private:
 		std::reference_wrapper<gl_video_device> m_video_out;
+		std::queue<command> m_commands;
+		std::chrono::steady_clock::time_point m_last_event;
 	};
 }
 
